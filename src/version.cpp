93e77334c1d64150ece11da8ef9a374c207692ea
WlanWerner <38541809+WlanWerner@users.noreply.github.com>
2018-10-16 19:15:25 +0200

    Update Client name
    Change satoshi to Jumpcoin

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both jumpcoind and jumpcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Jumpcoin");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
